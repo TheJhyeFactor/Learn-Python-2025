444037ff4658a03e9d7cdd941b4ebe3f0bea8b6b
TheJhyeFactor <aeo.dev00001@gmail>
2025-08-02 13:14:03 +1000

    added a * in the code

--- Programing-with-Mosh-PY-2025/Code/Hello-World.py
+++ Programing-with-Mosh-PY-2025/Code/Hello-World.py
@@ -2,3 +2,5 @@ print("Hello, World!")
 print("Welcome to Programming with Mosh - Python 2025!")
 print("This is a simple Python application.")
 print("Let's start coding!")
+
+print("this is going to x 10" * 10)
