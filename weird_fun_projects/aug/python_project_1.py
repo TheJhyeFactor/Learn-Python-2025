43c484c6135357fba6338f97d697b116d96653f9
TheJhyeFactor <aeo.dev00001@gmail.com>
2025-08-06 22:26:00 +1000

    Removed class while i look and research them a bit more as I think there was some miss understand in relation to understanding how to use them

--- weird_fun_projects/aug/python_project_1.py
+++ weird_fun_projects/aug/python_project_1.py
@@ -1,11 +1,24 @@
 ##### This project i am going to take a url from a user 
 #####I am going to then scan that URL to see what ports are open
 ##### Then i am going to tell the user what ports are open 
+import subprocess
+
+def user_input(url):
+        url = (input(f"Welcome what website do you want to scantoday:\n PLEASE NOTE MUST ENTER FULL URL e.g: https://XYZ.com\n"))
+        url_confirm = input(f"Are you sure you want to scan this website ?{url}\n Y(yes) | N(No)")
+        print(url_confirm)  
+        if url_confirm == "yes":
+            print("we are getting this far")
+            website_validation(url)
+        elif url_confirm == "no":
+            user_input()
+        
+        #print(self.url, self.url_confirm) #This is a debugging methoed to few input.
+
+def website_validation(url):
+        url = user_input(url_confirm)
+        ping = subprocess.run(["ping",(url)],capture_output=True,text=True)
+        print(ping.stdout)
 
 
-class website_scan:
-    url = (input(f"Welcome what website do you want to scantoday:\n PLEASE NOTE MUST ENTER FULL URL e.g: https://XYZ.com\n"))
-    url_confirm = input(f"Are you sure you want to scan this website ?{url}\n Y(yes) | N(No)")
-    print(url, url_confirm)
-
-website_scan
+user_input
