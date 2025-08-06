b1dee07cbae092767c651496b076c6d79c406db4
TheJhyeFactor <aeo.dev00001@gmail.com>
2025-08-06 23:23:06 +1000

    added in sockets to use so i can try and ping websites on ports.

--- weird_fun_projects/aug/python_project_1.py
+++ weird_fun_projects/aug/python_project_1.py
@@ -2,9 +2,28 @@
 #####I am going to then scan that URL to see what ports are open
 ##### Then i am going to tell the user what ports are open 
 import subprocess
+import socket
+
+socket_obj = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+
+ports = {
+    "FTP": 21,
+    "SSH": 22,
+    "Telnet": 23,
+    "HTTP": 80,
+    "HTTPS": 443,
+    "MySQL": 3306,
+    "PostgreSQL": 5432,
+    "http-alt / Proxies": 8080,
+    "https-alt / Panels": 8443,
+    "Elasticsearch": 9200,
+    "Redis": 6379,
+}
+
 
 def user_input():
-        url = (input(f"Welcome what website do you want to scantoday:\n PLEASE NOTE MUST ENTER FULL URL e.g: https://XYZ.com\n"))
+        url = (input(f"Welcome what website do you want to scantoday:\n PLEASE NOTE MUST ENTER FULL URL e.g: google.com\n"))
         url_confirm = input(f"Are you sure you want to scan this website ?{url}\n Y(yes) | N(No)")
         print(url)
         print(url_confirm)  
@@ -22,4 +41,9 @@ def website_validation(url):
         return print(ping.stdout)
 
 
-user_input()
+def port_scan(url,ports):
+    pass
+
+ip_con = socket_obj.connect(("google.com", 80))
+
+print(ip_con)
