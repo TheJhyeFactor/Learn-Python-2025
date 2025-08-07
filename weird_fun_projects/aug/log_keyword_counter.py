e0e587e27000022b315f04b71775b456adc0155d
TheJhyeFactor <aeo.dev00001@gmail.com>
2025-08-08 00:39:05 +1000

    This is going to be fun haha, I have now got the keyword storing it in a str & I am also using tkinter and getting the users file & its path storing it in the path var

---
+++ weird_fun_projects/aug/log_keyword_counter.py
@@ -0,0 +1,26 @@
+"""
+📋 Objective
+Write a small script that:
+
+Accepts:Path to a text “log” fileA keyword to search for
+
+Opens the file safely:Counts how many times that keyword appears (across all lines)
+
+Handles errors (e.g. file not found)
+Prints the final tally of total keywords
+"""
+
+import os
+import re
+
+from tkinter import Tk
+from tkinter.filedialog import askopenfilename
+path = askopenfilename(title='Select Folder') # shows dialog box and return the path
+print(path)  
+
+
+
+
+def keyword():
+    key = input(str("Please input the Keyword you would like to find"))
+    
