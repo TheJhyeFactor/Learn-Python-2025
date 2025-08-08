6e6a99a40e64a8338c328e1cfaf98e92dcd040b3
TheJhyeFactor <aeo.dev00001@gmail.com>
2025-08-09 00:29:53 +1000

    got it working to look and see if the request status code is in the reply_code dir then we print it along with the code if it aint then we print that code we didnt not have and let the user know, I am going to add in later a way to break down for the users if there was a page we found and it returned X code i will tell the user they should look into X page

--- weird_fun_projects/aug/tiny_directory_fuzzer.py
+++ weird_fun_projects/aug/tiny_directory_fuzzer.py
@@ -5,9 +5,36 @@ For each word, request /<word> using HTTP HEAD and report non-404 responses (e.g
 Optional: try simple extensions like .php, .bak, .old.
 
 """
-import subprocess
 import requests
 import time
+from bs4 import BeautifulSoup
+
+
+reply_code = {
+    200: "OK (exists)",
+    204: "No Content (exists)",
+    206: "Partial Content (exists)",
+    301: "Moved Permanently (redirect)",
+    302: "Found (redirect)",
+    303: "See Other (redirect)",
+    307: "Temporary Redirect",
+    308: "Permanent Redirect",
+    400: "Bad Request (exists, needs params?)",
+    401: "Unauthorized (auth required)",
+    403: "Forbidden (exists, blocked)",
+    405: "Method Not Allowed (try GET)",
+    410: "Gone (used to exist)",
+    429: "Too Many Requests (rate limited)",
+    500: "Internal Server Error",
+    501: "Not Implemented (e.g., HEAD unsupported)",
+    502: "Bad Gateway",
+    503: "Service Unavailable",
+    504: "Gateway Timeout",
+    404: "Not Found,This page does not exist"
+}
+
+
+r = [""]
 
 wrd_lst = [
     "admin","login","logout","signin","signup","account","user","users","profile",
@@ -33,12 +60,29 @@ def user_input():
 
 def url_scan(url,wrd_lst):
     for wrd in wrd_lst:
-        f_url = url + wrd
-        #print(f_url)
-        time.sleep(1.2)
-        r = requests.get(f"{f_url}{wrd}")
-        print(r.text)
+        
+        f_url = url + wrd # Url we use to request the head
+        r = requests.head(f"{f_url}")
+        
+        url_status = r.status_code       
+        
+        if r.status_code in reply_code:
+            print(f"This is the Final URL {f_url}: this is out request head Status code {r.status_code}, This is the status is: {url_status}")
+        else:
+            print(f"This Status code {r.status_code} Wasnt in the Dicatrnery of Reply codes {reply_code}")
+            print("we are still not able to cross check the status code with the reply codes key")
 
 
+
+
+               
         
 user_input()
+
+""""
+        time.sleep(5)
+        if r.status_code in reply_code.keys():
+            print(f"We are able to see the: {f_url}:\n{sat_code}")
+        else:
+            print(f"{f_url}:\nWe are unable to review the reply code")
+            """
