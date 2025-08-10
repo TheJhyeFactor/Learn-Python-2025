5ca7edd8dd6a55a94ffed52560ca855570713ca3
TheJhyeFactor <aeo.dev00001@gmail.com>
2025-08-10 14:29:32 +1000

    getting users file that they want to encrypt

--- weird_fun_projects/aug/file_encryptor.py
+++ weird_fun_projects/aug/file_encryptor.py
@@ -1,14 +1,23 @@
-import tkinter
+from tkinter import *
+from tkinter.ttk import *
+from tkinter.filedialog import askopenfilename
+from pathlib import Path
 from cryptography.fernet import Fernet
 import secrets
+
+
+
+def file_c():
+    file_path = askopenfilename(title="Choose your file")
+    print(file_path)
+
+
 key = Fernet.generate_key()  # secure random 16 bytes
 toke = Fernet(key)
 
 token = toke.encrypt(b"this is a secret message")
-
-token
-
 print(token, key)
-
-
 print(toke.decrypt(token))
+
+
+file_c()
