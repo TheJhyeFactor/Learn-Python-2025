6ee07adc1aa205d7f7c224d59131a44c3e60297d
TheJhyeFactor <aeo.dev00001@gmail.com>
2025-08-10 20:26:15 +1000

    expanding on the update_contats func, hanlding inputs but i should added better edgecase handling, I will once i have it wokring on this part

--- weird_fun_projects/aug/contact_book.py
+++ weird_fun_projects/aug/contact_book.py
@@ -18,8 +18,7 @@ def add_contact():
     firstname = input(str("Please input the name of person you want to add to your conatact list: "))
     lastname = input(str(f"Please enter the last name for your contact: "))
     number = input(str(f"Now please enter {firstname} {lastname}'s Number:  "))
-    c = input(f"Please confrim is the Contac details are correct: Name: {firstname} {lastname} \n Number: {number}\n Please enter Y for yes N for No\n: ")
-    confrim = c.lower()
+    c = input(f"Please confrim is the Contac details are correct: Name: {firstname} {lastname} \n Number: {number}\n Please enter Y for yes N for No\n: ").lower()
     if  confrim == "y":
         print("Contact added !!")
         contacts[2] = {
@@ -40,11 +39,26 @@ def add_contact():
 
 def update_contact():
     num_search = input(str("Please enter the Mobile number of the contact you are trying to update: "))
-    
     for contact_id, person in contacts.items():
         if person["number"] == num_search:
-            print(person)
-
+            num_upconfrim = input(f"Confrim contact to update: {person}\n Do you want to update this contact ? Y(yes) : N(no)").lower()
+            
+            if num_upconfrim == "y":
+                field = input(f"What field do you want to update: f(Firstname): L(Lastname): M(Mobile Number)\n Please enter the letter according to the field you want to update:- ").lower()
+                if field == "f":
+                    print("first")
+                elif field == "l":
+                        print("lastname")
+                elif field == "m":
+                    print("mobile")
+                else:
+                    print("invaild option")
+                    num_upconfrim
+            elif num_upconfrim == "n":
+                update_contact()
+            else:
+                print("Bruh just enter in the options I showed you haha.")
+                update_contact()
 
 def delete_user(firstname,number):
     pass
