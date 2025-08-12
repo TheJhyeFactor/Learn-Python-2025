fdbc36ac8c7ebcf1b76affa27eb154369943fe53
TheJhyeFactor <aeo.dev00001@gmail.com>
2025-08-12 22:18:33 +1000

    :GOAL So todays project is to make a simple keylogger

---
+++ weird_fun_projects/aug/keyboard_logger.py
@@ -0,0 +1,33 @@
+"""
+Goal:
+Build a Python script that listens for keyboard presses and logs each key to a text file in real time.
+"""
+
+from pynput import keyboard
+
+def on_press(key, injected):
+    try:
+        print('alphanumeric key {} pressed; it was {}'.format(
+            key.char, 'faked' if injected else 'not faked'))
+    except AttributeError:
+        print('special key {} pressed'.format(
+            key))
+
+def on_release(key, injected):
+    print('{} released; it was {}'.format(
+        key, 'faked' if injected else 'not faked'))
+    if key == keyboard.Key.esc:
+        # Stop listener
+        return False
+
+# Collect events until released
+with keyboard.Listener(
+        on_press=on_press,
+        on_release=on_release) as listener:
+    listener.join()
+
+# ...or, in a non-blocking fashion:
+listener = keyboard.Listener(
+    on_press=on_press,
+    on_release=on_release)
+listener.start()
