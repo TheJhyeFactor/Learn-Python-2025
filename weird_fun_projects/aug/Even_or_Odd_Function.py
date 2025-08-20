4bb53c4be252a94dda88efe0e06c67f087613428
TheJhyeFactor <aeo.dev00001@gmail.com>
2025-08-20 22:39:29 +1000

    SO it works haha all done

---
+++ weird_fun_projects/aug/Even_or_Odd_Function.py
@@ -0,0 +1,11 @@
+
+
+
+def check_number(num):
+    if num % 2 == 0:
+        print(f"Your Number is even: {num}")
+    else:
+        print(f"Your number is Odd; {num}")
+        
+        
+check_number(88)
