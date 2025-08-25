191b66f15df4fccb411f3ead93991abef4827b44
TheJhyeFactor <aeo.dev00001@gmail.com>
2025-08-26 00:20:37 +1000

    joined the tuple,  removed the specials from being a list to a tuple so that i could easily join them to make a master list for the radnom function so i only have to pass one sequence easier

--- weird_fun_projects/w1.py
+++ weird_fun_projects/w1.py
@@ -2,14 +2,27 @@ import random
 
 alphabet_both = tuple("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ")
 number = tuple("12345678910")
-special = ["!", "@", "$", "%", "&", "&"] 
+special = ("!", "@", "$", "%", "&", "&")
+
+master_lst = tuple(alphabet_both + number + special)
 
 
-def input_pas():
+def input_passlen():
     pass_len = int(input("Please enter in the length you want you password to be: For secuirty the min lenght is 6 char long:\nEnter Lenght: "))
+    
     if pass_len >= 6:
         print(f"{pass_len}")
     else:
-        print("you suck")
+        print(f"Hey bro not cool man, maybe you should retake maths becasue {pass_len} is smaller than 6 -_- \n Now i am going to give you another chance to get it right:\n")
+        input_passlen()
+        
 
-input_pas()
+    
+#def passowrd_gen():
+
+#random.choice()
+#pass
+
+
+
+print(master_lst)
