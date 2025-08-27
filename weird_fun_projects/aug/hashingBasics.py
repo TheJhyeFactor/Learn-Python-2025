c3b51a7c47e384f3a81d8ab60101029857e9c66f
TheJhyeFactor <aeo.dev00001@gmail.com>
2025-08-28 01:44:40 +1000

    So we are taking a input, from the user 'We will normalise it being either definng it being a str or int' But then we are enconding it in UTF-16 to account for special char and printing the bytes to validate it

---
+++ weird_fun_projects/aug/hashingBasics.py
@@ -0,0 +1,10 @@
+import hashlib
+
+ha = hashlib.sha256()
+
+message = input("Please input your meesage you would like to Hash: ")
+messageB = bytes(message, "utf-16")
+
+ha.update(messageB)
+
+print(messageB)
