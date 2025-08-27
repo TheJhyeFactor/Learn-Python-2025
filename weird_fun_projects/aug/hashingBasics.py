97ebc733023e3d5ffc2b44fb51fa675e84120c6a
TheJhyeFactor <aeo.dev00001@gmail.com>
2025-08-28 01:53:28 +1000

    I am way to tired for this ahha but i was on holiday not to long ago and just getting into the normal habbit of it again

--- weird_fun_projects/aug/hashingBasics.py
+++ weird_fun_projects/aug/hashingBasics.py
@@ -4,8 +4,7 @@ ha = hashlib.sha256()
 
 message = input("Please input your meesage you would like to Hash: ")
 messageB = bytes(message, "utf-16")
-
-ha.update(messageB)
+messageB = ha.hexdigest()
 
 print(messageB)
 
