efb9716ab035f15fbb78a43d8a16f1630641e81c
TheJhyeFactor <aeo.dev00001@gmail.com>
2025-08-30 22:41:08 +1000

    added a commit look at some code / docs not feeling well

--- weird_fun_projects/aug/systemTimeGreeting.py
+++ weird_fun_projects/aug/systemTimeGreeting.py
@@ -1,1 +1,5 @@
+
 #This is going to use sys time & if its x time in the Am good morining X time in the Afternoon good afternoon or X time goodnight, A basic if else statment
+
+
+print('this is going to say hello')
